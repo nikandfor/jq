e54e7d0481f6068faf23a6aee2177d4c801b76ed
nikandfor <nikandfor@gmail.com>
2024-09-05 02:33:33 +0200

    if

---
+++ if.go
@@ -0,0 +1,66 @@
+package jq
+
+import (
+	"fmt"
+)
+
+type (
+	If struct {
+		Cond, Then, Else Filter
+
+		cond, done   bool
+		cnext, dnext bool
+	}
+)
+
+func NewIf(c, t, e Filter) *If { return &If{Cond: c, Then: t, Else: e} }
+
+func (f *If) ApplyTo(b *Buffer, off int, next bool) (res int, more bool, err error) {
+	if !next {
+		f.done = false
+		f.cnext = false
+		f.dnext = false
+	}
+	if f.done {
+		return None, false, nil
+	}
+
+	if !f.dnext {
+		cond := csel(f.Cond != nil, f.Cond, Filter(Dot{}))
+
+		res, f.cnext, err = cond.ApplyTo(b, off, f.cnext)
+		if err != nil {
+			return off, false, err
+		}
+
+		f.cond = IsTrue(b, res)
+	}
+
+	var data Filter
+
+	if f.cond {
+		data = csel(f.Then != nil, f.Then, Filter(Dot{}))
+	} else {
+		data = csel(f.Else != nil, f.Else, Filter(Off(False)))
+	}
+
+	res, f.dnext, err = data.ApplyTo(b, off, f.dnext)
+	if err != nil {
+		return off, false, err
+	}
+
+	f.done = !(f.dnext || f.cnext)
+
+	return res, !f.done, nil
+}
+
+func (f If) String() string {
+	c := csel(f.Cond != nil, f.Cond, Filter(Dot{}))
+	t := csel(f.Then != nil, f.Then, Filter(Dot{}))
+
+	if f.Else == nil {
+		return fmt.Sprintf("if %v then %v end", c, t)
+	}
+
+	return fmt.Sprintf("if %v then %v else %v end", c, t, f.Else)
+}
---
+++ if_test.go
@@ -0,0 +1,21 @@
+package jq
+
+import "testing"
+
+func TestIfPipe(tb *testing.T) {
+	b := NewBuffer(nil)
+	off := b.appendVal(arr{false, 1, 2})
+
+	testIter(tb, NewPipe(NewIter(), NewIf(nil, NewObject("x", Dot{}), NewLiteral("no"))), b, off, []any{
+		"no", obj{"x", 1}, obj{"x", 2},
+	})
+}
+
+func TestIfIter(tb *testing.T) {
+	b := NewBuffer(nil)
+	off := b.appendVal(arr{false, 1, 2})
+
+	testIter(tb, NewIf(NewIter(), NewObject("x", Dot{}), NewLiteral("no")), b, off, []any{
+		"no", obj{"x", arr{false, 1, 2}}, obj{"x", arr{false, 1, 2}},
+	})
+}
